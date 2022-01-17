52e8673b6fef96e2ae2b4adaa278ec44093c25a1
xem <maxime.euziere@gmail.com>
2022-01-17 22:38:54 +0100

    361

--- history.js
+++ history.js
@@ -67,3 +67,5 @@ x=y=a=[];for(C of O.value)++x,C<" "?x=!++y:C>" "&&a.push({x,y,C,w:"#"==C,u:0,v:0
 x=y=a=[];for(C of O.value)++x,C<" "?x=!++y:C>" "&&a.push({x,y,C,w:"#"==C,u:0,v:0});M=o=>a.map(p=>{p.t=.1;p.s=0;o||(p.D=p.w);for(q of a)(g=Math.hypot(e=p.x-q.x,f=p.y-q.y)/2-1)<0?o?(g/=p.D,p.t+=(f*(z=3-p.D-q.D)+p.v-q.v)*g,p.s+=(e*z+p.u-q.u)*g):p.D+=g*g:1});setInterval`M(c.width^=M());for(p of a)with(p)c.getContext("2d").fillText(C,3*x,5*y),w||(y+=v+=t/4,x+=u+=s/2)`
 
 x=y=a=[];for(C of O.value)++x,C<" "?x=!++y:C>" "&&a.push({x,y,C,w:"#"==C,u:0,v:0});M=o=>a.map(p=>{p.t=.1;p.s=0;o||(p.D=p.w);a.map(q=>(g=Math.hypot(e=p.x-q.x,f=p.y-q.y)/2-1)<0?o?(g/=p.D,p.t+=(f*(z=3-p.D-q.D)+p.v-q.v)*g,p.s+=(e*z+p.u-q.u)*g):p.D+=g*g:1)});setInterval`M(c.width^=M());for(p of a)with(p)c.getContext("2d").fillText(C,3*x,5*y),w||(y+=v+=t/4,x+=u+=s/2)`
+
+_=a=[];for(C of O.value)_++,C>" "&&a.push({x:_%80,y:_/80|0,C,w:"#"==C,u:0,v:0});M=o=>a.map(p=>{p.t=.1;p.s=0;o||(p.D=p.w);a.map(q=>(g=Math.hypot(e=p.x-q.x,f=p.y-q.y)/2-1)<0?o?(g/=p.D,p.t+=(f*(z=3-p.D-q.D)+p.v-q.v)*g,p.s+=(e*z+p.u-q.u)*g):p.D+=g*g:1)});setInterval`M(c.width^=M());for(p of a)with(p)c.getContext("2d").fillText(C,3*x,5*y),w||(y+=v+=t/4,x+=u+=s/2)`
